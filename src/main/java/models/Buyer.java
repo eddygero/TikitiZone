078d01479056f71920149f915a471a405431aa65
felixmaina <karani.maina2010@gmail.com>
2022-02-15 11:08:57 +0300

    generate  a constructor and getters  en setters

--- src/main/java/models/Buyer.java
+++ src/main/java/models/Buyer.java
@@ -1,4 +1,37 @@
 package models;
 
 public class Buyer {
+    String name;
+    String  age;
+    String ticket;
+
+    public Buyer(String name, String age, String ticket) {
+        this.name = name;
+        this.age = age;
+        this.ticket = ticket;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getTicket() {
+        return ticket;
+    }
+
+    public void setTicket(String ticket) {
+        this.ticket = ticket;
+    }
 }
