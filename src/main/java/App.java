6fddf61570fdd921c8257b4f6d27444858f8b75b
TrekHub <teddyomondi001@gmail.com>
2022-02-16 09:47:55 +0300

    cleaning the code

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,33 +1,6 @@
-import spark.ModelAndView;
-import spark.template.handlebars.HandlebarsTemplateEngine;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import  static  spark.Spark.*;
 public class App {
 
 
-    public  static  void main(String[] args){
-        staticFileLocation("/public");
 
-        //Root Route
-        get("/", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            return  new ModelAndView(model, "index.hbs");
-        }, new HandlebarsTemplateEngine());
-
-        //login Route
-        get("/login", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            return  new ModelAndView(model, "login.hbs");
-        }, new HandlebarsTemplateEngine());
-
-        //signup Route
-        get("/signup", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            return  new ModelAndView(model, "signup.hbs");
-        }, new HandlebarsTemplateEngine());
-
-    }
 }
