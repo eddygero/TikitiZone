20671b25b7810b6c606d74806dcac7ff24b4faed
felixmaina <karani.maina2010@gmail.com>
2022-02-18 15:42:11 +0300

    deploed the app successfully to heroku

--- src/main/java/models/DB.java
+++ src/main/java/models/DB.java
@@ -3,5 +3,7 @@ package models;
 import org.sql2o.Sql2o;
 
 public class DB {
-    public static Sql2o sql2o=new Sql2o("jdbc:postgresql://localhost:5432/tikitizone", "karani-dev", "felixmaina");
+   // public static Sql2o sql2o=new Sql2o("jdbc:postgresql://localhost:5432/tikitizone", "karani-dev", "felixmaina");
+     public static Sql2o sql2o=new Sql2o("jdbc:postgresql://ec2-3-219-204-29.compute-1.amazonaws.com:5432/dtusk1vjb0tsm?sslmode=require", "zpxyzibabprqqc", "3aea9356868fddcdc3f6a3b72c8623be4054a64eeffed4375d843d02c2750777");
+
 }
